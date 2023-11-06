a4e7b9a9ef6a8ebca83f6b2bfb4b969791f7e557
dmorton714 <102087408+dmorton714@users.noreply.github.com>
2023-11-06 10:27:40 -0500

    finished

--- js/asyncscript.js
+++ js/asyncscript.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', async () => {
   try {
     // Fetch the processed_data from the local file
-    const fetchedData = await fetch('json_files/processed_data.json')
+    const fetchedData = await fetch('json_files/processed_data.json');
     const data = await fetchedData.json();
-    const restaurants = JSON.parse(data).sort((a, b) => (b.score - a.score));
+    const restaurants = JSON.parse(data);
+    const restaurantsRatingFilter = JSON.parse(data).filter((item) => item.Rating !== null);
+
     // Function to create a chart
-    const createChart = (elementId, labels, data) => {
+    const createChart = (elementId, labels, data, label) => {
       const chartCtx = document.getElementById(elementId).getContext('2d');
       new Chart(chartCtx, {
         type: 'bar',
         data: {
-            labels: labels,
+            labels,
             datasets: [{
-            label: 'Scores',
-            data: data,
+            label,
+            data,
             backgroundColor: '#FF9800',
             borderWidth: 1
             }]
@@ -39,21 +41,31 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
       });
     };
-    
+
     // Create the chart for the top 10 restaurants
     // const topScores = restaurants.slice(0, 10).map(restaurant => restaurant.score);
     // const topLabels = restaurants.slice(0, 10).map(restaurant => restaurant.EstablishmentName);
     // createChart('topChart', topLabels, topScores);
 
     // Create the chart for the worst 10 restaurants
-    const topScores = restaurants.slice(-10).map(restaurant => restaurant.Rating).reverse();
-    const topLabels = restaurants.slice(-10).map(restaurant => restaurant.EstablishmentName).reverse();
-    createChart('topChart', topLabels, topScores);
+    const ratedWorst = restaurantsRatingFilter.sort((a, b) => (a.Rating - b.Rating)).slice(0, 10);
+    const ratedNames = ratedWorst.map((item) => item.EstablishmentName);
+    const ratedRatings = ratedWorst.map((item) => item.Rating);
+    createChart('topChart', ratedNames, ratedRatings, 'Yelp Ratings');
+    // const topScores = restaurants.slice(-10).map(restaurant => restaurant.Rating).reverse();
+    // const topLabels = restaurants.slice(-10).map(restaurant => restaurant.EstablishmentName).reverse();
+    // createChart('topChart', topLabels, topScores);
+
 
     // Create the chart for the worst 10 restaurants
-    const worstScores = restaurants.slice(-10).map(restaurant => restaurant.score).reverse();
-    const worstLabels = restaurants.slice(-10).map(restaurant => restaurant.EstablishmentName).reverse();
-    createChart('worstChart', worstLabels, worstScores);
+    const sortedByScores = restaurants.sort((a, b) => (a.score - b.score)).slice(0, 10);
+    const scoredNames = sortedByScores.map((item) => item.EstablishmentName);
+    const scoredScores = sortedByScores.map((item) => item.score);
+    createChart('worstChart', scoredNames, scoredScores, 'Scores');
+    // const worstScores = restaurants.slice(-10).map(restaurant => restaurant.score).reverse();
+    // const worstLabels = restaurants.slice(-10).map(restaurant => restaurant.EstablishmentName).reverse();
+    // createChart('worstChart', worstLabels, worstScores);
+
 
     const searchInput = document.getElementById('searchInput');
     const searchButton = document.getElementById('searchButton');
@@ -76,19 +88,19 @@ document.addEventListener('DOMContentLoaded', async () => {
         return 0;
       });
       // Display search results
-      filteredRestaurants.forEach((restaurant) => {
+      filteredRestaurants.forEach((data) => {
         const listItem = document.createElement('li');
         const nameElement = document.createElement('span');
-        nameElement.textContent = `${restaurant.EstablishmentName} - ${restaurant.score}`;
+        nameElement.textContent = `${data.EstablishmentName} - Health Score: ${data.score} - Yelp Rating: ${data.Rating || 'N/A'} - Number of Yelp Reviews: ${data.ReviewCount || 'N/A'}`; 
         const addressElement = document.createElement('span');
-        addressElement.textContent = restaurant.Address;
+        addressElement.textContent = data.Address;
         listItem.appendChild(nameElement);
         listItem.appendChild(document.createElement('br'));
         listItem.appendChild(addressElement);
         searchResults.appendChild(listItem);
       });
     };
-
+    
     // Event listener for Enter key press in search input field
     searchButton.addEventListener('click', performSearch);
     searchInput.addEventListener('keydown', event => {
